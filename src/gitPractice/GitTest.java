bddb3751ec2e3ea6e27f418a45840fb938ede9c7
swapnil mane <maneswapnil885@gmail.com>
2021-10-23 22:42:24 -0700

    this is first commit

---
+++ src/gitPractice/GitTest.java
@@ -0,0 +1,16 @@
+package gitPractice;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+public class GitTest {
+	static WebDriver driver;
+
+	@Test
+	public void Test1() {
+		System.out.println("this is first case");
+		
+		
+	}
+
+}
